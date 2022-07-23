495c8b2357271d4b738b91f86ae7ddf52d8e4b06
shantothomas341 <shanto.thomas@prvak.in>
2022-07-23 08:31:26 +0530

    v1 release

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wordconverter.wsgi --log-file -
---
+++ converterapp/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+from django.shortcuts import render
+from num2words import num2words
+import re
+
+def landingpage(request):
+    return render(request, "landingpage.html")
+
+
+def converter(request):
+    if request.method == 'POST':
+        number = request.POST.get('number')
+        if not re.match('^[1-9]\d*$', str(number)):
+            return render(request, "landingpage.html", {"result": "INVALID NUMBER"})
+
+        print(num2words(int(number)))
+
+        return render(request, "landingpage.html", {"result": num2words(int(number)).upper()})
+
+    return render(request, "landingpage.html")
